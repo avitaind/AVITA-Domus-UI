fa97897b05cca3b8c1c00f73f4a426ae79336258
avitaind <avitaind@gmail.com>
2022-02-09 16:35:46 +0530

    speedo meter range slider

--- js/script.js
+++ js/script.js
@@ -1,44 +1,101 @@
-
-/*Function 1*/
-
-$(document).ready( function(){
-    $('#toggle-button').click( function() {
-        var toggleDisplay = $("#toggle").display() == "none" ? "block" : "none";
-        $('#toggle').animate({ display: toggleDisplay });
-    });
-});
-
-// jQuery v3.3.1 is supported
-$("#arc-slider").roundSlider({
+/*Brightness*/
+$("#slider-bright").roundSlider({
+    radius: 80,
+    circleShape: "half-top",
     sliderType: "min-range",
-    circleShape: "custom-quarter",
-    value: 0,
-    startAngle: 50,
-    editableTooltip: true,
-    radius: 360,
-    width: 2,
-    handleSize: "+32",
-    tooltipFormat: function (args) {
-    return args.value + " %";
-    }
+    showTooltip: false,
+    value: 50
 });
 
-/*Function 2*/
 
-function fun2(){
-    console.log("Function 2 clicked");
-}
+$(document).ready(function () {
+    // Toggles paragraphs display
+    $(".toggle-bright").click(function () {
+        $(".brightness").toggle();
+        document.getElementById("slider-preset").style.display="none";
+        document.getElementById("slider-color").style.display="none";
+        document.getElementById("slider-timer").style.display="none";
+
+        // $(".toggle-btn").toggle();
+
+    });
+
+    
+});
+
+/*Presets*/
+$("#slider-preset").roundSlider({
+    radius: 80,
+    circleShape: "half-top",
+    sliderType: "min-range",
+    showTooltip: false,
+    value: 50
+});
+
+$(document).ready(function () {
+    // Toggles paragraphs display
+    $(".toggle-preset").click(function () {
+        $(".presets").toggle();
+        // $(".toggle-btn").toggle();
+        document.getElementById("slider-bright").style.display="none";
+        document.getElementById("slider-color").style.display="none";
+        document.getElementById("slider-timer").style.display="none";
+
+    });
+
+    
+});
 
 
-/*Function 3*/
 
-function fun3(){
-    console.log("Function 3 clicked");
-}
+/*Colors*/
+$("#slider-color").roundSlider({
+    radius: 80,
+    circleShape: "half-top",
+    sliderType: "min-range",
+    showTooltip: false,
+    value: 50
+});
 
 
-/*Function 4*/
 
-function fun4(){
-    console.log("Function 4 clicked");
-}
+$(document).ready(function () {
+    // Toggles paragraphs display
+    $(".toggle-color").click(function () {
+        $(".colors").toggle();
+        // $(".toggle-btn").toggle();
+        document.getElementById("slider-preset").style.display="none";
+        document.getElementById("slider-bright").style.display="none";
+        document.getElementById("slider-timer").style.display="none";
+
+    });
+
+    
+});
+
+
+
+/*Timers*/
+$("#slider-timer").roundSlider({
+    radius: 80,
+    circleShape: "half-top",
+    sliderType: "min-range",
+    showTooltip: false,
+    value: 50
+});
+
+
+$(document).ready(function () {
+    // Toggles paragraphs display
+    $(".toggle-timer").click(function () {
+        $(".timers").toggle();
+        // $(".toggle-btn").toggle();
+        document.getElementById("slider-preset").style.display="none";
+        document.getElementById("slider-color").style.display="none";
+        document.getElementById("slider-bright").style.display="none";
+
+    });
+
+    
+});
+
