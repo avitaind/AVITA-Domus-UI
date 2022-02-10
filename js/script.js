4a6f791498f262e1244956b960c4e7a4948ec791
avitaind <avitaind@gmail.com>
2022-02-10 12:32:05 +0530

    two functions completed

--- js/script.js
+++ js/script.js
@@ -4,32 +4,46 @@ $("#slider-bright").roundSlider({
     circleShape: "half-top",
     sliderType: "min-range",
     showTooltip: false,
-    value: 50
+    value: 50,
+    change: function (args) {
+        // console.log(args.value);
+        // document.getElementById("main").style.opacity = args.value/100 ;
+        $('body').css('opacity', args.value/100);
+
+     }        
 });
 
 
-$(document).ready(function () {
-    // Toggles paragraphs display
-    $(".toggle-bright").click(function () {
-        $(".brightness").toggle();
-        document.getElementById("slider-preset").style.display="none";
-        document.getElementById("slider-color").style.display="none";
-        document.getElementById("slider-timer").style.display="none";
 
-        // $(".toggle-btn").toggle();
-
+    $(document).ready(function () {
+        // Toggles paragraphs display
+        $(".toggle-bright").click(function () {
+            $(".brightness").toggle();
+            document.getElementById("slider-preset").style.display="none";
+            document.getElementById("slider-color").style.display="none";
+            document.getElementById("slider-timer").style.display="none";
+            // $(".toggle-btn").toggle();
+        });
     });
 
-    
-});
 
 /*Presets*/
 $("#slider-preset").roundSlider({
     radius: 80,
     circleShape: "half-top",
     sliderType: "min-range",
+    min:0,
+    max:255,
     showTooltip: false,
-    value: 50
+    value: 50,
+    change: function (args) {
+        red = args.value;
+        green = args.value;
+        blue = args.value;
+        color = "#" + ((1 << 255) + (red << 16) + (green << 8) + blue).toString(16).slice(1);
+        console.log(color);
+        document.getElementById("main").style.background = color;
+    }    
 });
 
 $(document).ready(function () {
